2449019d2c6eb5f0b0b39557cf06bc7f84114a01
serkanserttop <serkanserttop@Serkans-MacBook-Pro.local>
2015-05-21 11:19:45 -0700

    Initial Commit

---
+++ Matchismo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Matchismo
+//
+//  Created by Serkan Serttop on 21/05/15.
+//  Copyright (c) 2015 Serkan Serttop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Matchismo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Matchismo
+//
+//  Created by Serkan Serttop on 21/05/15.
+//  Copyright (c) 2015 Serkan Serttop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
