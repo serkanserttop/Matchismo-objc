975c12d0012ea48281b6cfc5e2658fc789aadbaa
Serkan Serttop <serkanserttop@yahoo.com>
2015-06-24 01:16:17 +0300

    Added CardMatchingGame, episode 3, slide 56

---
+++ Matchismo/CardMatchingGame.h
@@ -0,0 +1,13 @@
+//
+//  CardMatchingGame.h
+//  Matchismo
+//
+//  Created by Serkan Serttop on 24/06/15.
+//  Copyright (c) 2015 Serkan Serttop. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CardMatchingGame : NSObject
+
+@end
---
+++ Matchismo/CardMatchingGame.m
@@ -0,0 +1,13 @@
+//
+//  CardMatchingGame.m
+//  Matchismo
+//
+//  Created by Serkan Serttop on 24/06/15.
+//  Copyright (c) 2015 Serkan Serttop. All rights reserved.
+//
+
+#import "CardMatchingGame.h"
+
+@implementation CardMatchingGame
+
+@end
